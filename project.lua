aaa7e175bc6862b48a8208abb115c084bdcf1289
thepigcat <benpospo@gmail.com>
2024-02-02 17:41:20 +0100

    Initial commit

---
+++ project.lua
@@ -0,0 +1,12 @@
+-- versioning
+Name = "surtur-test-3"
+Props = {
+    std = "c17",
+    version = "0.1",
+    type = "lib"
+}
+
+-- external dependencies
+Dependencies = {
+    -- { "dependency_name", 0.1 }
+}
---
+++ src/lib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "add.h"
+
+int main(void) {
+    printf("Result: %d\n", add(10, 30));
+}
